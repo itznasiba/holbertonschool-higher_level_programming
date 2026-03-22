55515384a30d8d36395212daed1fcb774660c504
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-22 19:59:20 +0000

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,6 +4,5 @@ def element_at(my_list, idx):
     """Retrieves an element from a list at a specific position."""
     # Check if the index is negative or out of the upper bounds
     if idx < 0 or idx >= len(my_list):
-        return None
-    
+        return None 
     return my_list[idx]
