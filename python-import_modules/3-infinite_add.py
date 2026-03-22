1339d778fc3df27bdbf3993075764c05e91d5434
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-22 19:41:36 +0000

    1

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    # Iterate through all arguments except the script name
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    
+    print("{}".format(total))
