d9cb44476ddc296906ca62d7af1b6a4dbd8e3f4e
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-22 19:48:17 +0000

    1

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     # Iterate through all arguments except the script name
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
-    print("{}".format(total))
+print("{}".format(total))
