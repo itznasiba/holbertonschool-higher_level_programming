7fb4dd53be2e8c0569ce0f236241c5a35fd57eca
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-28 15:39:14 +0000

    4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,10 +3,10 @@
 def only_diff_elements(set_1, set_2):
     new_set = []
     set_u = []
-     for i in set_1:
-         for j in set_2:
-             if i == j:
-                 new_set.append(i)
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                new_set.append(i)
     for i in set_1:
         if i in new_set:
             continue
