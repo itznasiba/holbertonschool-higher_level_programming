c4bb05b5d92e2e3563cfee0d0726d8bdae9700fd
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-28 19:16:15 +0000

    1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     m = None
     ky = None
     for key, value in a_dictionary.items():
-        if m = None or value > m:
+        if m == None or value > m:
             m = value
             ky = key
     return ky
