2b6ebd03c274f6b73b3d2a4c4b21e0090bd406a0
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-28 15:14:33 +0000

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-
+    summ = 0
+    new_list = []
+    for i in my_list:
+        if i in new_list:
+            continue
+        else:
+            new_list.append(i)
+            summ = summ + i
+    return summ
