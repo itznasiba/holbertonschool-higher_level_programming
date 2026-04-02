fb88bbd7d2fafa1978ab2a14bf82b94bd97a679a
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-02 13:51:12 +0000

    pythin classes 0 square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+this module defines an empty square
+"""
+
+
+class Square:
+    """
+    empty class to define square
+    """
+    def __init__(self, size):
+        self.__size__=size
