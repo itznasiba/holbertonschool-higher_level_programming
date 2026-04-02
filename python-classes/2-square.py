201ee93dba146b18669b6f066552ec02c7490bb7
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-02 13:58:35 +0000

    pythin classes 0 square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     """
     empty class to define square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
