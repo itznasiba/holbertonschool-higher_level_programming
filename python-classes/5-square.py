a0f0f76dfa38ff3515cae66e62e35bf342d54825
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-03 11:56:54 +0000

    1

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+this module defines an empty square
+"""
+
+
+class Square:
+    """
+    empty class to define square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        size property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the value of size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size)**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range (self.__size):
+            for j in range (self.__size):
+                print("#", end="")
+            print()
