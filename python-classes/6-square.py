f41c4bed1946ea7317fc77271a6481594e1249fe
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-03 13:28:24 +0000

    1

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,7 +11,7 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.__position = position
+        self.__self_position = position
 
     @property
     def size(self):
