23cbaa524bab747065dbdc2923a05381d8cb5ef9
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-05 19:03:20 +0000

    1

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -53,6 +53,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return self.__width*self.__height
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
