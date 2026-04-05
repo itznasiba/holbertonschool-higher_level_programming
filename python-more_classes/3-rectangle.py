c8b210b9a223eead84c6a730dcc971933af6cdf1
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-05 19:21:41 +0000

    1

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -60,7 +60,9 @@ class Rectangle:
         return 2*(self.__width + self.__height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle using # characters."""
+        """
+        Returns a string representation of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         rect_lines = ["#" * self.__width for _ in range(self.__height)]
