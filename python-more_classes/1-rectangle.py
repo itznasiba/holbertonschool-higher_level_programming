72b59f99022561c0184bbc402c33034fb4d1a80e
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-05 18:22:49 +0000

    1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+this module defines an empty square
+"""
+
+
+class Square:
+    """
+    empty class to define square
+    """
+    def __init__(self, size=0, position=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self, value):
+        """
+        returns the value of width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        sets the value of width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self, value):
+        """
+        returns the value of height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        sets the value of height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
