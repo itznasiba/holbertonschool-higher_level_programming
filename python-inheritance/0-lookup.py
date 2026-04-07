69050cbc6c2c655a8a28b09cd409b61a3f3ebdaa
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-07 19:32:42 +0000

    0 look up

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+function for returning all functions of object
+"""
+
+def lookup(obj):
+    """
+    function for returning all attributes and methods
+    """
+    return dir(obj)
