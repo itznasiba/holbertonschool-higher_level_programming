b97245a948e56583f6196e927de3ea9b99bf0438
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-12 12:35:33 +0000

    1

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+this module defines an empty square
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        initialize rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+        def area(self):
+            """
+            calculates area
+            """
+            return (self.__size)**2
+
+        def __str__(self):
+            """
+            str underscore functions
+            """
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
