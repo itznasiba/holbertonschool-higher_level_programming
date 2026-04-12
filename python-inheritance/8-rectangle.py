1f4cd2d43f0f8144b89e1d1a8f82e5160b4e761f
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-12 12:27:30 +0000

    1

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+this module defines an empty square
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    rectangle class
+    """
+
+    def __init__(self, width, height):
+        """
+        initialize rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+
