f1a1d1efda92e70b0a0419480175d38aed685d75
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-12 10:42:23 +0000

    1

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+code for checing if obj is intance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if obj is instance of a_class
+    """
+
+    return type(obj) is a_class
