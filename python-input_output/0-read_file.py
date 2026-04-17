0270974ad8ada84d2dfab8d5aadc2f9bb42a3e36
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-17 14:21:49 +0000

    1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+reads text file utf 8
+"""
+
+
+def read_file(filename=""):
+    """
+    read file function
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
