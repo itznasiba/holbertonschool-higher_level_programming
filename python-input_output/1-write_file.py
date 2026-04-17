a4280eccdcf7c961aca26398d5482ab7849c229d
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-17 14:29:51 +0000

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+writing function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file function
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
