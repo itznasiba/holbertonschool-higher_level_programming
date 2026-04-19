6028e772b15532eaf31457e2888150c9e3d4390f
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-19 19:57:32 +0000

    1

---
+++ python-input_output/task_00_basic_serialization.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for basic serialization and deserialization of Python dictionaries.
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary and saves it to a JSON file.
+
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The name of the output JSON file.
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """
+    Loads and deserializes data from a JSON file to recreate Python dictionary
+
+    Args:
+        filename (str): The name of the input JSON file.
+
+    Returns:
+        dict: The deserialized Python dictionary.
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
