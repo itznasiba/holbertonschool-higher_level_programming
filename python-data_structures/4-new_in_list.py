2a6e2f8ddd8e951024991d509fcc405033f3b7e0
itznasiba <zeynallinesibe2006@gmail.com>
2026-06-02 18:37:09 +0200

    1

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     """
-    Replaces an element in a list at a specific position 
+    Replaces an element in a list at a specific position
     without modifying the original list.
     """
     # Check if the index is out of bounds (negative or exceeding list length)
@@ -10,8 +10,8 @@ def new_in_list(my_list, idx, element):
 
     # Create a shallow copy of the original list
     new_list = my_list.copy()
-    
+
     # Replace the element at the specified index
     new_list[idx] = element
-    
+
     return new_list
